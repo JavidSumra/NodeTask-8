77ed824bd408c67a977ad65f7ef01f1ee9f6a7a8
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-01-16 22:51:27 +0530

    Update and rename userdetail.js to user.js

---
+++ models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Todo, {
+        foreignKey: "userId",
+      });
+    }
+  }
+  User.init({
+    FirstName: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    LastName:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    password:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    email:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
