2ef114df897902c24f655a1fba94d331d2cf2eea
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-20 19:50:15 +0530

    Updated App With Cron Job

---
+++ nodemailer.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+const node = require("nodemailer");
+require("dotenv").config();
+// const random = Math.floor(1000+Math.random()*9000);
+module.exports = async (email, subject, text) => {
+  const transporter = node.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.email,
+      pass: process.env.Password,
+    },
+  });
+
+  let messagedetail = {
+    from: process.env.email,
+    to: email,
+    subject: subject,
+    text: text,
+  };
+
+  let send = await transporter.sendMail(messagedetail, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email Sent Successfully");
+    }
+  });
+};
