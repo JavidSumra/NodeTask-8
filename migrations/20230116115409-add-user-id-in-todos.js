e05f8a9659d39e295bde7f9abe3d9841d9bdcf35
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-20 19:53:31 +0530

    Updated App with Cron Jon

--- migrations/20230116115409-add-user-id-in-todos.js
+++ migrations/20230116115409-add-user-id-in-todos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 module.exports = {
@@ -7,7 +6,7 @@ module.exports = {
       type: Sequelize.DataTypes.INTEGER,
     });
 
-    await queryInterface.addConstrain("Todos", {
+    await queryInterface.addConstraint("Todos", {
       fields: ["userId"],
       type: "foreign key",
       refrences: {
@@ -15,24 +14,6 @@ module.exports = {
         field: "id",
       },
     });
-=======
-'use strict';
-
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn("Todos","userId",{
-      type:Sequelize.DataTypes.INTEGER,
-    })
-
-    await queryInterface.addConstrain("Todos",{
-      fields:["userId"],
-      type:"foreign key",
-      refrences:{
-        table:"Users",
-        field:"id",
-      }
-    })
->>>>>>> 9a2e8d135357aa45d441ac3ac3f212b32e238270
     /**
      * Add altering commands here.
      *
@@ -41,22 +22,7 @@ module.exports = {
      */
   },
 
-<<<<<<< HEAD
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("Todos", "userId");
-=======
-  async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Todos","userId")
->>>>>>> 9a2e8d135357aa45d441ac3ac3f212b32e238270
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-<<<<<<< HEAD
   },
-=======
-  }
->>>>>>> 9a2e8d135357aa45d441ac3ac3f212b32e238270
 };
